7c5757e388b23b21acf3858d237d6e505a225b33
davidgonzalezfx <dvdramos16@gmail.com>
2021-10-21 21:02:00 -0500

    Run project

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
   });
 
   return (
-    <AppearanceProvider>
+    <AppearanceProvider> 
       <WalkthroughScreen
         appConfig={WalkthroughAppConfig}
         appStyles={DynamicAppStyles}
